a231e3f66a77e64c25605a4bd1b03b616f9bad37
CindyKatoni <katmuema@gmail.com>
2021-01-19 11:33:24 +0300

    form function

---
+++ script.js
@@ -0,0 +1,6 @@
+let century = document.getElementById("century").value;
+let year = document.getElementById("year").value;
+let month = document.getElementById("month").value;
+let day = document.getElementById("day").value;
+let male = document.getElementById("male").value;
+let female = document.getElementById("female").value
