608b610a70fcf4e8748f4d4b5f50706bdf36ae4c
CindyKatoni <katmuema@gmail.com>
2021-01-19 12:19:56 +0300

    splice year

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
-let century = document.getElementById("century").value;
-let year = document.getElementById("year").value;
-let month = document.getElementById("month").value;
-let day = document.getElementById("day").value;
-let male = document.getElementById("male").value;
-let female = document.getElementById("female").value
+function akanGenerator() {
+
+    let year = document.getElementById("year").value;
+    let month = document.getElementById("month").value;
+    let day = document.getElementById("day").value;
+    let male, female;
+
+    if (document.getElementById("male-input").checked == true) {
+        male = "male"
+    };
+    if (document.getElementById("female-input").checked == true) {
+        female = "female"
+    };
+
+    //
+    let daysOfWeek = [
+        "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+    ];
+
+    let maleAkanNames = [
+        "Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"
+    ];
+
+    let femaleAkanNames = [
+        "Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"
+    ];
+
+    //Formula for calculating akan names
+
+}
